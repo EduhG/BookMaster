f8f3550f66fa1fd2b98e39e923ab8aab1f395a72
EduhG <edwinnyangena@gmail.com>
2016-08-24 09:25:31 +0300

    Added button to main screen

--- app/src/main/java/com/example/eduhg/bookmaster/MainActivity.java
+++ app/src/main/java/com/example/eduhg/bookmaster/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.eduhg.bookmaster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView mainTextView;
+    Button mainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("Set in Java");
+
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        mainTextView.setText("Button pressed!");
     }
 }
